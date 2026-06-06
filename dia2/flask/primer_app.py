0fbac350fe6cf7ab922ea4b2cff172ab6f414c01
Abel Guevara <55357845+AbelG9@users.noreply.github.com>
2026-06-05 20:14:33 -0500

    added saludar y sumar functions

--- dia2/flask/primer_app.py
+++ dia2/flask/primer_app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<center><h1>Hola Mundo con Flask</h1></center>'
+
+@app.route('/saludo')
+def saludo():
+    nombre = request.args.get('nombre','')
+    return f"<h1>Hola {nombre}</h1>"
+
+@app.route('/sumar/<int:a>/<int:b>')
+def sumar(a,b):
+    resultado = a + b
+    return f"<center><h1>La suma de {a} + {b} es {resultado} </h1></center>"
 
 app.run(debug=True)
